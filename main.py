881e48fa37ac4664c5bf025d2dc51c7a9ff7f4d2
Johannes Seitz <johannes.seitz@st.oth-regensburg.de>
2020-11-18 14:52:14 +0100

    Durchsuchen hinzugefügt

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
-import csv
+# mport csv
 from qtpy import QtWidgets
+from qtpy.QtWidgets import QFileDialog
 
 from ui_pcdaten.mainwindow import Ui_MainWindow
+
 
 app = QtWidgets.QApplication(sys.argv)
 
@@ -13,6 +15,12 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.ui_pcdaten = Ui_MainWindow()
         self.ui_pcdaten.setupUi(self)
+        self.ui_pcdaten.pushButton.clicked.connect(self.browsefiles)
+
+    def browsefiles(self):
+        # Problem: Wie kann der Standard Suchpfad so eingestellt werden, dass er direkt in Downloads landet
+        fname = QFileDialog.getOpenFileName(self, 'Open file', 'C:\%Username%\Downloads')
+        self.ui_pcdaten.lineSearch.setText(fname[0])
 
 
 window = MainWindow()
